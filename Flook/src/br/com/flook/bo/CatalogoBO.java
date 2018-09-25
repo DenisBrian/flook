a36595617422bf5d1eea38f60b4ecf1a48406090
FelipeHonorio <38380183+FelipeHonorio@users.noreply.github.com>
2018-09-25 09:28:00 -0300

    Java Doc

--- Flook/src/br/com/flook/bo/CatalogoBO.java
+++ Flook/src/br/com/flook/bo/CatalogoBO.java
@@ -2,9 +2,28 @@ package br.com.flook.bo;
 
 import br.com.flook.beans.Catalogo;
 import br.com.flook.dao.CatalogoDAO;
-
+/**
+* Responável por todas as Regras de negócio e padronizações do CatalogoBO
+* 1°) A quantidades de caracteres da descrição não pode ser igual a 0
+* 2°) A quantidades de caracteres da descrição não pode ser maior que 50
+* @author DENIS BRIAN CANOLA
+* @author FELIPE HONÓRIO CAMARGO
+* @author RAFAEL MACHADO CARDOSO
+* @author MATHEUS APRIGIO DA SILVA
+* @author JEFFREY RODRIGUES INNOCENCIO
+* @version 1.0
+* @since 1.0
+* @see br.com.flook.beans.Catalogo
+* @see br.com.flook.dao.CatalogoDAO
+*/
 public class CatalogoBO {
-	
+	/**
+	 * Este método ira validar e repassar o objeto Catalogo para a classe CatalogoDAO
+	 * @param obj Este parâmetro representa um objeto Catalogo do Beans.
+	 * @return O método retorna um numero com o codigo do Curso.
+	 * @throws Exception Tratado a exceção checked SQLExceptio
+	 * @author DENIS BRIAN CANOLA
+	 */
 	public static int novoCatalogo(Catalogo obj) throws Exception{
 		
 		if(obj.getDescricao().length() == 0)
@@ -19,6 +38,13 @@ public class CatalogoBO {
 		
 		return cod;
 	}
+	/**
+	 * Este método ira pesquisar o Catalogo pelo codigo
+	 * @param cod Este parâmetro representa o codigo do objeto Catalogo 
+	 * @return O método retorna os Catalogo encontrados
+	 * @throws Exception Tratado a exceção checked SQLExceptio
+	 * @author DENIS BRIAN CANOLA
+	 */
 	public static Catalogo obterPorCodigo(int cod) throws Exception{		
 		CatalogoDAO dao = new CatalogoDAO();
 		Catalogo obj = dao.obter(cod);
