beff54b612e68b4e09f5c7a46918c2c3ba1ff0dc
FelipeHonorio <38380183+FelipeHonorio@users.noreply.github.com>
2018-09-25 09:53:30 -0300

    Java Doc

--- Flook/src/br/com/flook/dao/CatalogoDAO.java
+++ Flook/src/br/com/flook/dao/CatalogoDAO.java
@@ -6,16 +6,34 @@ import java.sql.ResultSet;
 
 import br.com.flook.beans.Catalogo;
 import br.com.flook.conexao.Conexao;
-
+/**
+ * Classe responsável por manipular a tabela T_FLO_CATALOGO.
+ * @author DENIS BRIAN CANOLA
+ * @version 1.0
+ * @since 1.0
+ * @see br.com.fiap.beans.Catalogo
+ * @see br.com.fiap.bo.CatalogoBO
+ */
 public class CatalogoDAO {
 	private Connection con;
 	private PreparedStatement ps;
 	private ResultSet rs;
-	
+	/**
+	 * Construtor responsável por abrir a conexão
+	 * @throws Exception Exceção checked SQLExceprion
+	 * @author DENIS BRIAN CANOLA
+	 */
 	public CatalogoDAO() throws Exception{
 		con = Conexao.conectar();
 	}
 	
+	/**
+	 * Adiciona uma tuba na tabela T_FLO_CATALOGO
+	 * @param obj Este parâmetro recebe um objeto Catalogo beans
+	 * @return retorna um Int com codigo 0
+	 * @throws Exception Exceção checked SQLExption
+	 * @author DENIS BRIAN CANOLA
+	 */
 	public int gravar(Catalogo obj) throws Exception{	
 		String _sql = "INSERT INTO T_FLO_CATALOGO (DS_CATALOGO) VALUES (?)";
 		
@@ -34,6 +52,14 @@ public class CatalogoDAO {
 		}		
 		return cod;
 	}	
+	
+	/**
+	 * Busca uma tuba na tabela T_FLO_CATALOGO
+	 * @param cod Este parâmetro refere-se ao Id do objeto Catalogo beans
+	 * @return retorna o objeto Catalogo beans
+	 * @throws Exception Exceção checked SQLExption
+	 * @author DENIS BRIAN CANOLA
+	 */
 	public Catalogo obter(int cod) throws Exception{
 		String _sql = "SELECT CD_CATALOGO, DS_CATALOGO FROM T_FLO_CATALOGO WHERE CD_CATALOGO = ?";
 		
@@ -51,6 +77,11 @@ public class CatalogoDAO {
 		return obj;
 	}
 
+	/**
+	 * Metodo que faz o fechamento da conexão com o banco de dados.
+	 * @throws Exception Exceção checked SQLExption
+	 * @author DENIS BRIAN CANOLA
+	 */
 	public void fechar() throws Exception{
 		con.close();
 	}
