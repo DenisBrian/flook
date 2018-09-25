c716c9290168b9c1f1dcf92b02cc70ef42f1b460
FelipeHonorio <38380183+FelipeHonorio@users.noreply.github.com>
2018-09-25 09:30:10 -0300

    Java Doc

--- Flook/src/br/com/flook/bo/CertificadoBO.java
+++ Flook/src/br/com/flook/bo/CertificadoBO.java
@@ -3,9 +3,30 @@ import java.util.List;
 
 import br.com.flook.beans.Certificado;
 import br.com.flook.dao.CertificadoDAO;
-
+/**
+* Responável por todas as Regras de negócio e padronizações do Certificado
+* 1°) A quantidades de caracteres do atributo certificado não pode ser igual 0 nem meno que 0
+* 2°) O codigo da Curso não poder ser igual a 0
+* 3°) O codigo da Instituição não poder ser igual a 0
+* 4°) O codigo da Usuario não poder ser igual a 0
+* @author DENIS BRIAN CANOLA
+* @author FELIPE HONÓRIO CAMARGO
+* @author RAFAEL MACHADO CARDOSO
+* @author MATHEUS APRIGIO DA SILVA
+* @author JEFFREY RODRIGUES INNOCENCIO
+* @version 1.0
+* @since 1.0
+* @see br.com.flook.beans.Certificado
+* @see br.com.flook.dao.CertificadoBO
+*/
 public class CertificadoBO {
-	
+	/**
+	 * Este método ira validar e repassar o objeto Certificado para a classe CertificadoBO
+	 * @param obj Este parâmetro representa um objeto Curso do Beans
+	 * @return O método retorna um valor booleano
+	 * @throws Exception Tratado a exceção checked SQLExceptio
+	 * @author DENIS BRIAN CANOLA
+	 */
 	public static boolean novoCertificado(Certificado obj) throws Exception{
 		
 		if(obj.getCertificado().length() == 0 || obj.getCertificado().length() > 0)
@@ -26,6 +47,13 @@ public class CertificadoBO {
 		return result;
 		
 	}
+	/**
+	 * Este método ira o obter o Certificado por Usuario
+	 * @param cod Este parâmetro representa o codigo do objeto Certificado
+	 * @return O método retorna os Certificados encontrados referente ao Usuário
+	 * @throws Exception Tratado a exceção checked SQLExceptio
+	 * @author DENIS BRIAN CANOLA
+	 */
 	public static List<Certificado> obterCertificadoPorUsuario(int cod) throws Exception{
 		CertificadoDAO dao = new CertificadoDAO();
 		List<Certificado> certificados = dao.obterPorUsuario(cod);
