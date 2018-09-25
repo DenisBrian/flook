2caf0d506f907066abfce2614d6ad0c79b80f806
FelipeHonorio <38380183+FelipeHonorio@users.noreply.github.com>
2018-09-25 09:43:56 -0300

    Java Doc

--- Flook/src/br/com/flook/bo/InstituicaoCursoBO.java
+++ Flook/src/br/com/flook/bo/InstituicaoCursoBO.java
@@ -4,9 +4,26 @@ import java.util.List;
 
 import br.com.flook.beans.InstituicaoCurso;
 import br.com.flook.dao.InstituicaoCursoDAO;
-
+/**
+ * Responsavel por todas as regras de negócios e padronizações da InstituiçãoCurso
+ * 1°) A quantidades de caracteres do nome não pode ser igual a 0
+ * @author FELIPE HONÓRIO CAMARGO
+ * @author DENIS BRIAN CANOLA
+ * @author RAFAEL MACHADO CARDOSO
+ * @author MATHEUS APRIGIO DA SILVA
+ * @author JEFFREY RODRIGUES INNOCENCIO
+ * @version 1.0
+ * @since 1.0
+ * @see br.com.flook.dao.InstituicaoCursoDAO
+ */
 public class InstituicaoCursoBO {
-	
+	/**
+	 * Este método ira buscar as a instutuição pelo nome do curso.
+	 * @param nome Este parâmetro representa um objeto do Beans.
+	 * @return O método retorna lista com as intituições.
+	 * @throws Exception Tratado a exceção checked SQLExceptio
+	 * @author DENIS BRIAN CANOLA
+	 */
 	public static List<InstituicaoCurso> obterPorNomeDeCurso(String nome) throws Exception{
 		
 		if(nome.length() == 0)
